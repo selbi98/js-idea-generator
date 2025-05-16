fc36b745d348f175b67f042d16e3b3b4da49f926
Selbi Dowletowa <sdowletowa4@gmail.com>
2025-05-16 12:40:50 +0500

    manifest.json

---
+++ assets/servise-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'leisure-ideas-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json',
+  //'/icon-192x192.png',  //Убедитесь, что эти иконки существуют.
+  //'/icon-512x512.png'
+];
+
+// Установка Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Открыт кэш:', CACHE_NAME);
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Обработка запросов
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response; 
+        }
+        return fetch(event.request); 
+      })
+  );
+});
+
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName); 
+          }
+        })
+      );
+    })
+  );
+});
